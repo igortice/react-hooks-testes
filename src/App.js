32e6420ad151af47cd4e46b97e78d1e8d69a73b0
Igor Rocha <igor.rocha@fortaleza.ce.gov.br>
2020-02-10 14:39:01 -0300

    better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const Exemplo1 = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `Counter: ${count}`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
+
+const Lista = ({ tarefas }) => {
+  const lista = tarefas.map((t, i) => <li key={i}>{t}</li>);
+
+  return lista.length ? <ol>{lista}</ol> : 'Sem tarefas cadastradas';
+};
+
+const Exemplo2 = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+
+  const handleClickButton = () => {
+    if (inputValue) {
+      setTarefas([...tarefas, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <Lista tarefas={tarefas} />
+
+      <div>
+        <input
+          onChange={e => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <button onClick={handleClickButton}>Nova Tarefa</button>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>oi</div>;
+  return (
+    <div>
+      <Exemplo1 />
+      <hr />
+      <Exemplo2 />
+    </div>
+  );
 };
 
 export default App;
