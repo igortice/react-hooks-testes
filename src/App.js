148fb3b8bdabbbd1c9a026b3027d2e454305ac5d
Igor Rocha <igor.rocha@fortaleza.ce.gov.br>
2020-02-10 15:45:05 -0300

    better

--- src/App.js
+++ src/App.js
@@ -1,50 +1,6 @@
-import React, { useState, useEffect } from 'react';
-
-const Exemplo1 = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    document.title = `Counter: ${count}`;
-  });
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  );
-};
-
-const Lista = ({ tarefas }) => {
-  const lista = tarefas.map((t, i) => <li key={i}>{t}</li>);
-
-  return lista.length ? <ol>{lista}</ol> : 'Sem tarefas cadastradas';
-};
-
-const Exemplo2 = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [tarefas, setTarefas] = useState([]);
-
-  const handleClickButton = () => {
-    if (inputValue) {
-      setTarefas([...tarefas, inputValue]);
-      setInputValue('');
-    }
-  };
-
-  return (
-    <div>
-      <Lista tarefas={tarefas} />
-
-      <div>
-        <input
-          onChange={e => setInputValue(e.target.value)}
-          value={inputValue}
-        />
-        <button onClick={handleClickButton}>Nova Tarefa</button>
-      </div>
-    </div>
-  );
-};
+import React from 'react';
+import Exemplo1 from './components/Exemplo1';
+import Exemplo2 from './components/Exemplo2';
 
 const App = () => {
   return (
---
+++ src/components/Exemplo2.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+const Lista = ({ tarefas, onClick }) => {
+  const lista = tarefas.map((t, i) => (
+    <li key={i}>
+      {t} | <button onClick={() => onClick(t)}>x</button>
+    </li>
+  ));
+
+  return lista.length ? <ol>{lista}</ol> : 'Sem tarefas cadastradas';
+};
+
+const Exemplo2 = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+
+  const handleClickButton = () => {
+    if (inputValue) {
+      setTarefas(prevState => [...prevState, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleRemove = val => {
+    var array = [...tarefas];
+    var index = array.indexOf(val);
+    if (index !== -1) {
+      array.splice(index, 1);
+      setTarefas(array);
+    }
+  };
+
+  return (
+    <div>
+      <Lista tarefas={tarefas} onClick={handleRemove} />
+
+      <div>
+        <input
+          onChange={e => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <button onClick={handleClickButton}>Nova Tarefa</button>
+      </div>
+    </div>
+  );
+};
+
+export default Exemplo2;
