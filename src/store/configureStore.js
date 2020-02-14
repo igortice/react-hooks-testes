63e0b9154c8102f6042ccb23383cf03e572ed867
Igor Rocha <igor.rocha@fortaleza.ce.gov.br>
2020-02-14 15:00:05 -0300

    add devtools react

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import exemploReducer from './reducers/exemploReducer';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({ exemplo: exemploReducer });
 
-export default () => createStore(rootReducer, applyMiddleware(thunk));
+export default () =>
+  createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
